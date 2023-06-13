33d782c5eaf4820a1d93ea6bc1071b5143507d05
Documenter.jl <documenter@juliadocs.github.io>
2023-06-13 18:46:47 +0000

    build based on 16ee517

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = NMEAParser","category":"page"},{"location":"#NMEAParser","page":"Home","title":"NMEAParser","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NMEAParser.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [NMEAParser]","category":"page"}]
+}
